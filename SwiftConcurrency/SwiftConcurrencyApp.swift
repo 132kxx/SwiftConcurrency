7fff1aa332f419c7e19989daf1d47ec70de71c4f
132kxx <132kxx@gmail.com>
2023-06-24 20:44:53 +0900

    Initial Commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by kxx on 2023/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
